ef11b32b036cd40560f8fac87a5f9e63c7aaac2c
Abner Rodrigues <abnerluis1001@gmail.com>
2020-08-06 15:50:16 -0300

    feat: created nested navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -16,6 +16,9 @@ function Landing() {
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
   }
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
 
   return (
     <View style={styles.container}>
@@ -27,7 +30,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
