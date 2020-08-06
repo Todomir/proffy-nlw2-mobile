533e2e48aa2d917c8e8b5981654ab6d9206ddd61
Abner Rodrigues <abnerluis1001@gmail.com>
2020-08-06 11:19:40 -0300

    feat: created Landing page component

--- App.tsx
+++ App.tsx
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Landing from './src/pages/Landing';
+
+import { AppLoading } from 'expo';
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from '@expo-google-fonts/archivo';
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <Text>Programmed to work and not to feel.</Text>
-      <StatusBar style='auto' />
-    </View>
+    <>
+      <Landing />
+      <StatusBar style='light' />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
