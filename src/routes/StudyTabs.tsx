e15c23b1b64fd4b6a8652f1d27a46c722eb27d93
Abner Rodrigues <abnerluis1001@gmail.com>
2020-08-06 16:04:23 -0300

    feat: styled tabs from StudyTabs

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import Favorites from '../pages/GiveClasses/Favorites';
 import TeacherList from '../pages/TeacherList';
@@ -8,9 +9,54 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 function StudyTabs() {
   return (
-    <Navigator>
-      <Screen name='TeacherList' component={TeacherList} />
-      <Screen name='Favorites' component={Favorites} />
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: '#fafafc',
+        inactiveTintColor: '#c1bccc',
+        activeBackgroundColor: '#ebebf5',
+        activeTintColor: '#32264d',
+      }}
+    >
+      <Screen
+        name='TeacherList'
+        component={TeacherList}
+        options={{
+          tabBarLabel: 'Proffys',
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons color={color} size={size} name='ios-easel' />;
+          },
+        }}
+      />
+      <Screen
+        name='Favorites'
+        component={Favorites}
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons color={color} size={size} name='ios-heart' />;
+          },
+        }}
+      />
     </Navigator>
   );
 }
